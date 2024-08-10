eeddf6091380c0ad5373885617fb96f4ee4f927c
vincyy24 <vaibhav.k.codes@gmail.com>
2024-08-10 21:30:08 +0530

    ✨ Integrated ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
 	return (
 		<>
-			<h1>Hello, World!</h1>{" "}
+			<h1>Hello, World!</h1>
+			<ButtonGroup>
+				<Button colorScheme="blue">Click Me!</Button>
+				<Button colorScheme="purple">Click Me!</Button>
+				<Button colorScheme="green">Click Me!</Button>
+				<Button colorScheme="yellow">Click Me!</Button>
+				<Button colorScheme="orange">Click Me!</Button>
+				<Button colorScheme="red">Click Me!</Button>
+			</ButtonGroup>
 		</>
 	);
 }
